9112f00fe8ec6fce5312440546ea31b2e8e6e2c2
Nikolay <freshitftm@gmail.com>
2021-03-19 11:47:21 +0200

    Add if example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,5 @@
-// alert('Hello world!');
-// alert('Bye world!');
+let data = '';
 
-const helloStr = 'Hello world!';
-let test = 'hello';
-
-let boolVariable = true;
-
-console.log(helloStr);
-
-console.log(test, typeof test);
-test = 5;
-console.log(test, typeof test);
-
-console.log(boolVariable, typeof boolVariable);
-// console.log('Hello world!');
-// console.log(10);
-
-// console.log(true);
-
-// console.log(null);
-// console.log(undefined);
+if(data){
+  console.log("Success");
+}

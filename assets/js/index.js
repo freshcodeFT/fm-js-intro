88b6c6fed362bab6b588205375f8e7d07c07f315
Nikolay <freshitftm@gmail.com>
2021-03-24 12:04:25 +0200

    Add switch example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,36 @@
-// const userInput = prompt("Enter the message: ");
+const userInput = +prompt("Выберите напиток:\n1 - Кофе\n 2 - Чай\n 3 - Сок ");
 
-// alert(userInput);
-
-// const userInput1 = prompt("Enter number 1: ");
-// const userInput2 = prompt("Enter number 2: ");
-
-// const getSumOfTwo = function(val1, val2){
-//   const result = Number(val1) + Number(val2);
-//   return isNaN(result) ? null : result;
-// }
-
-// const sum = getSumOfTwo(userInput1, userInput2);
-// console.log(sum);
-
+switch (userInput) {
+  case 1:{
+    console.log("Кофе");
+    break;
+  }
+  case 2:{
+    console.log("Чай");
+    break;
+  }
+  case 3:{
+    console.log("Сок");
+    break;
+  }
+  case 4:
+  case 5:
+  case 6: {
+    console.log("Секретный напиток");
+    break;
+  }
+  default: {
+    console.log("Такого напитка нет");
+  }
+}
 
 /*
-  1. Функция, которая вернёт максимальное значение из 2.
-      Значения брать у пользователя.
-  2. Функция, которая вернёт минимальное значение из 2.
-      Значения брать у пользователя.
-
-  3. Функция, которая определяет чётность числа.
-  
-  (*4. Функция, которая решает квадратные уравнения)
+  С помощью switch:
+  В переменной day лежит какое-то число из интервала от 1 до 31
+  Определите в какую декаду месяца попадает это число 
+  (в первую, вторую или третью).
 */
 
-/**
- * Solves quadric equations
- * @param {number} a
- * @param {number} b 
- * @param {number} c 
- * @returns {false | null | [number, number]} - returns false on invalid input
- */
-function quadricEquation(a, b, c){
-  if( isNaN(a - b - c) ) return false;
-  if(a === 0) return null;
-
-  const d = b ** 2 - 4 * a * c;
-  if(d < 0) return null;
-  if(d === 0) {
-    const x = -b / (2 * a);
-    return [x, x];
-  }
-
-  const x1 = (-b + d ** (1 / 2)) / (2 * a);
-  const x2 = (-b - d ** (1 / 2)) / (2 * a);
-  return [x1, x2];
-}
-
-const result = quadricEquation(1, 8, 12);
-if(!result) {
-  console.log("Ошибка вычислений");
-} else {
-  console.log("x1 =", result[0], "x2 =", result[1]);
-}
+/*
+  Напишите калькулятор. 2 Числа и операцию спрашивать у пользователя.
+*/

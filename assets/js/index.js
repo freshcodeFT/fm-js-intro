45c9dc0fac9c9812b55e0a39b7c9a2e430e93408
Nikolay <freshitftm@gmail.com>
2021-03-25 09:48:41 +0200

    Add funcs example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,9 @@
-const userInput = +prompt("Выберите напиток:\n1 - Кофе\n 2 - Чай\n 3 - Сок ");
-
-switch (userInput) {
-  case 1:{
-    console.log("Кофе");
-    break;
-  }
-  case 2:{
-    console.log("Чай");
-    break;
-  }
-  case 3:{
-    console.log("Сок");
-    break;
-  }
-  case 4:
-  case 5:
-  case 6: {
-    console.log("Секретный напиток");
-    break;
-  }
-  default: {
-    console.log("Такого напитка нет");
-  }
+//func expression
+const test1 = function(params){
+  console.log(1);
 }
 
-/*
-  С помощью switch:
-  В переменной day лежит какое-то число из интервала от 1 до 31
-  Определите в какую декаду месяца попадает это число 
-  (в первую, вторую или третью).
-*/
-
-/*
-  Напишите калькулятор. 2 Числа и операцию спрашивать у пользователя.
-*/
+//func declaration
+function test2(params) {
+  console.log(2);
+}

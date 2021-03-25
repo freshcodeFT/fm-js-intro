b43cd8e82dfb7abb47181e1f330befdb7a13565f
Nikolay <freshitftm@gmail.com>
2021-03-25 10:17:44 +0200

    Add deterministic func example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,27 @@
-//func expression
-const test1 = function(params){
-  console.log(1);
-}
+'use strict';
+let global1 = 5;
+let global2 = 5;
+
+console.log(test('test: msg'));
+console.log(deterministic('determ: msg',global2));
+
+console.log(test('test: msg'));
+console.log(deterministic('determ: msg',global2));
+
+global1 *= 2;
+global2 *= 2;
+
+console.log(test('test: msg'));
+console.log(deterministic('determ: msg',global2));
 
 //func declaration
-function test2(params) {
-  console.log(2);
+function test(msg) {
+  const result = global1 += 2;
+  return msg + result;
+}
+
+
+function deterministic(msg, local){
+  const result = local += 2;
+  return msg + result;
 }

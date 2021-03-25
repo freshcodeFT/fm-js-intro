8e799c9ab8cc4718682b8c45b75978014aa64579
Nikolay <freshitftm@gmail.com>
2021-03-25 12:15:41 +0200

    Add high order function usage example

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,11 @@
 
 let numA = +prompt("Write a: ");
 let numB = +prompt("Write b: ");
+const operator = prompt("Write operator");
+
+const calculationResult = calculate(numA, numB, operator);
+console.log(calculationResult);
+
 
 //pure func
 function sum(a, b){
@@ -23,9 +28,47 @@ function div(a, b){
   return a / b;
 }
 
-const highOrderFunc = function (valA, valB, operationFunc){
-  const result = operationFunc(valA, valB);
-  return result;
+function calculate(numA, numB, operator){
+
+  const highOrderFunc = function (valA, valB, operationFunc){
+    const result = operationFunc(valA, valB);
+    console.log(operationFunc);
+    return result;
+  }
+
+  let res = null;
+
+  switch(operator){
+    case '+': {
+      res = highOrderFunc(numA, numB, sum);
+      break;
+    }
+    case '-': {
+      res = highOrderFunc(numA, numB, sub);
+      break;
+    }
+    case '*': {
+      res = highOrderFunc(numA, numB, mul);
+      break;
+    }
+    case '/': {
+      res = highOrderFunc(numA, numB, div);
+      break;
+    }
+    default: {
+      console.log("Wrong operator");
+      break;
+    }
+  }
+  return res;
 }
-const countedValue = highOrderFunc(numA, numB, mul);
-console.log("Result: ", countedValue);
+
+// console.log(highOrderFunc(2,5,"*"));
+/*
+  Создать или доработать калькулятор с использовнием switch так,
+  чтобы при выборе операции вызывалась функция высшего порядка,
+  в которой вызывается функция, которая соответствует текущей операции (sum, sub, mul, div)
+  
+  В функции высшего порядка вывести в консоль функцию, которая передается в качестве параметра.
+
+*/

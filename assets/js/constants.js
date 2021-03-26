4905e06c6f8f5e58f6b9ea4d21215cbf1d4856d0
Nikolay <freshitftm@gmail.com>
2021-03-26 11:46:40 +0200

    Add factorial example

---
+++ assets/js/constants.js
@@ -0,0 +1,2 @@
+const GOOD_PASSWORD = "qwerty123";
+const MAX_TRIES = 5;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,38 @@
-'use strict';
+"use strict";
 
-let counter = 0;
+/*
+  1. Спрашивать у пользователя пароль до тех пор, 
+    пока он не введет правильный.
+    1.1. Ограничить кол-во попыток до 5.
+*/
 
-while (counter <= 10) {
-  counter++;
-  if(counter % 2 > 0){
-    continue;
+// let triesCounter = 0;
+
+// while (triesCounter < MAX_TRIES) {
+//   const userPass = prompt("Enter your password: ");
+//   if (userPass === GOOD_PASSWORD) {
+//     console.log("Success");
+//     break;
+//   }
+//   console.log("Error");
+//   triesCounter++;
+// }
+
+/* 2. Найти факториал числа */
+
+function factorial(num) {
+  if( num < 0 ) return null;
+  
+  let factCounter = 1;
+  let countedFactorial = BigInt(1);
+
+  while (factCounter < num) {
+    factCounter++;
+    countedFactorial *= BigInt(factCounter);
   }
-  console.log(counter);
- 
+  return countedFactorial;
 }
+const factNumber = +prompt("Enter number: ");
+console.log(factorial(factNumber));
 
-console.log("End of loop");
+/* 3. Возвести число в заданную степень (power) с помощью цикла */

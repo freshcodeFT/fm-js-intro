b4afc9ad06f7137cee40ca0fd87a153ecddf63d1
Nikolay <freshitftm@gmail.com>
2021-03-26 10:13:10 +0200

    Add while continue example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,74 +1,14 @@
 'use strict';
 
-let numA = +prompt("Write a: ");
-let numB = +prompt("Write b: ");
-const operator = prompt("Write operator");
+let counter = 0;
 
-const calculationResult = calculate(numA, numB, operator);
-console.log(calculationResult);
-
-
-//pure func
-function sum(a, b){
-  return a + b;
-}
-
-//pure func
-function sub(a, b){
-  return a - b;
-}
-
-//pure func
-function mul(a, b){
-  return a * b;
-}
-
-//pure func
-function div(a, b){
-  return a / b;
-}
-
-function calculate(numA, numB, operator){
-
-  const highOrderFunc = function (valA, valB, operationFunc){
-    const result = operationFunc(valA, valB);
-    console.log(operationFunc);
-    return result;
+while (counter <= 10) {
+  counter++;
+  if(counter % 2 > 0){
+    continue;
   }
-
-  let res = null;
-
-  switch(operator){
-    case '+': {
-      res = highOrderFunc(numA, numB, sum);
-      break;
-    }
-    case '-': {
-      res = highOrderFunc(numA, numB, sub);
-      break;
-    }
-    case '*': {
-      res = highOrderFunc(numA, numB, mul);
-      break;
-    }
-    case '/': {
-      res = highOrderFunc(numA, numB, div);
-      break;
-    }
-    default: {
-      console.log("Wrong operator");
-      break;
-    }
-  }
-  return res;
+  console.log(counter);
+ 
 }
 
-// console.log(highOrderFunc(2,5,"*"));
-/*
-  Создать или доработать калькулятор с использовнием switch так,
-  чтобы при выборе операции вызывалась функция высшего порядка,
-  в которой вызывается функция, которая соответствует текущей операции (sum, sub, mul, div)
-  
-  В функции высшего порядка вывести в консоль функцию, которая передается в качестве параметра.
-
-*/
+console.log("End of loop");

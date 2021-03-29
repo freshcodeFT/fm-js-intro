e731cba7180a457920ad86888ada879d5e44c9b3
Nikolay <freshitftm@gmail.com>
2021-03-29 09:31:55 +0300

    Add do while example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,9 @@
 "use strict";
 
-/*
-  1. Спрашивать у пользователя пароль до тех пор, 
-    пока он не введет правильный.
-    1.1. Ограничить кол-во попыток до 5.
-*/
-
-// let triesCounter = 0;
-
-// while (triesCounter < MAX_TRIES) {
-//   const userPass = prompt("Enter your password: ");
-//   if (userPass === GOOD_PASSWORD) {
-//     console.log("Success");
-//     break;
-//   }
-//   console.log("Error");
-//   triesCounter++;
-// }
-
-/* 2. Найти факториал числа */
-
-function factorial(num) {
-  if( num < 0 ) return null;
-
-  let factCounter = 1;
-  let countedFactorial = BigInt(1);
-
-  while (factCounter < num) {
-    factCounter++;
-    countedFactorial *= BigInt(factCounter);
-  }
-  return countedFactorial;
+while(false){
+  console.log(1);
 }
-// const factNumber = +prompt("Enter number: ");
-// console.log(factorial(factNumber));
 
-/* 3. Возвести число в заданную степень (power) с помощью цикла */
-
-function getPower(num, power){
-  if(power === 0){
-    return 1;
-  }
-  if(power < 0) {
-    return 1 / getPower(num, -power);
-    // return 1 / (num ** -power)
-  }
-
-  let result = num;
-  let powerCounter = 1;
-  while(powerCounter < power){
-    result *= num;
-    powerCounter++;
-  }
-
-  return result;
-}
+do{
+  console.log(2);
+} while (false);

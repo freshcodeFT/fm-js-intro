d53883fe60f187e972630e7da9f85a70713f383d
Nikolay <freshitftm@gmail.com>
2021-03-30 11:28:13 +0300

    Add counting params example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,30 @@
 'use strict';
 
-/* Создать функцию, которая возвращает валидный url */
-
-const PROTOCOL = 'http://';
-const PROTOCOL_SAFE = 'https://';
-const SERVER_IP = 'localhost';
-const PORT = 3000;
-
-function createQuery(absPath = '/home', isSafe = true ){
-  return `${isSafe ? PROTOCOL_SAFE : PROTOCOL}${SERVER_IP}:${PORT}${absPath}`;
+const user = {
+  name: 'John',
+  'favorite color': 'red',
+  0: 'testData',
+  1: 'unique text',
+  '5test': '5testValue',
 }
+
+user.name = 'Bob';
+console.log(user['5test']);
+
+const param = 'favorite color';
+
+console.log(user[param]);
+console.log(user.name);
+
+console.log(user['0']);
+console.log(user['1']);
+console.log(user[0]);
+console.log(user[1]);
+
+user[0] = 'test something';
+user['1'] = 'test nothing';
+
+user.age = 18;
+user['isMale'] = true;
+
+console.log(user);

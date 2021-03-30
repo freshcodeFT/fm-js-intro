73130c3308311e1676d5ac8256e16456d4cc6fec
Nikolay <freshitftm@gmail.com>
2021-03-30 09:25:31 +0300

    Add template string example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,11 @@
 'use strict';
-/*
+
 const a = +prompt('num1');
 const b = +prompt('num2');
 
-alert(a + ' + ' + b + ' = ' + (a + b));
-*/
+//alert(a + ' + ' + b + ' = ' + (a + b));
+alert(`${a} + ${b} = ${a + b} `);
+
 
 console.log("tst \"sffdsfds\" fdgfd\n 'sdfsdfsd' ");
 console.log('sdfjksdf "sdfsdf" df\'gdf\n sdfsd');

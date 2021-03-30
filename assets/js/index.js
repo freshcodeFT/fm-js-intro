f33391768ee488e4a4dd2793a26b11aff6b62d6c
Nikolay <freshitftm@gmail.com>
2021-03-30 12:02:02 +0300

    Add multiplication table example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,18 @@
 'use strict';
 
-const user = {
-  name: 'John',
-  'favorite color': 'red',
-  0: 'testData',
-  1: 'unique text',
-  '5test': '5testValue',
+
+/* 
+  Создать таблицу умножения
+  Разместить её в объекте. 
+  При этом ключ - само выражение, а значение - результат умножения
+*/
+function createMultiplicationTable(){
+  const table = {};
+  for(let i = 1; i <= 10; i++){
+    for(let j = 1; j <= 10; j++){
+      table[`${i} * ${j}`] = i * j;
+    }
+  }
+
+  return table;
 }
-
-user.name = 'Bob';
-console.log(user['5test']);
-
-const param = 'favorite color';
-
-console.log(user[param]);
-console.log(user.name);
-
-console.log(user['0']);
-console.log(user['1']);
-console.log(user[0]);
-console.log(user[1]);
-
-user[0] = 'test something';
-user['1'] = 'test nothing';
-
-user.age = 18;
-user['isMale'] = true;
-
-console.log(user);

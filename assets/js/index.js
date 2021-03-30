478bbad1c4c16c709b01578d30e03c87dd604f30
Nikolay <freshitftm@gmail.com>
2021-03-30 11:01:00 +0300

    Add url creation example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,12 @@
 'use strict';
 
-const user = {
-  firstName: 'Test',
-  lastName: 'Testovich',
-  age: 18,
-  isMale: true,
-  sayHello: function(){
-    alert(`Hello! My name is ${this.firstName} ${this.lastName}.`)
-  },
-  isAdult: function(){
-    return this.age >= 18;
-  },
+/* Создать функцию, которая возвращает валидный url */
+
+const PROTOCOL = 'http://';
+const PROTOCOL_SAFE = 'https://';
+const SERVER_IP = 'localhost';
+const PORT = 3000;
+
+function createQuery(absPath = '/home', isSafe = true ){
+  return `${isSafe ? PROTOCOL_SAFE : PROTOCOL}${SERVER_IP}:${PORT}${absPath}`;
 }
-
-const User = function(firstName, lastName, age, isMale){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.isMale = isMale;
-
-  this.sayHello = function(){
-    alert(`Hello! My name is ${this.firstName} ${this.lastName}.`)
-  }
-  this.isAdult = function(){
-    return this.age >= 18;
-  }
-}
-
-const user1 = new User("John", "Doe", 19, true);
-const user2 = new User("Elizabeth", "Smith", 17, false);

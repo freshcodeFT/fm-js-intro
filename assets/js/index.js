07ce7fe461f3413a88df5d81b0d685e0411519e2
Nikolay <freshitftm@gmail.com>
2021-03-31 10:15:22 +0300

    Add link example

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,3 +20,28 @@ arr.push(7,8,9,10);
 console.log(userTels);
 console.log(arr);
 
+function MyArray(){
+  this.length = 0;
+  this.push = function push(item) {
+    this[this.length++] = item;
+    return this.length;
+  }
+}
+
+const myArr = new MyArray();
+
+const t1 = 1;
+const t2 = 1;
+
+const obj1 = {divan: 'black'};
+const obj2 = {divan: 'black'};
+
+const link1 = obj1;
+
+console.log(obj1 === obj2); // false
+console.log(link1 === obj2) // false
+console.log(obj1 === link1); // true
+
+link1.table = "brown";
+
+console.log(obj1);

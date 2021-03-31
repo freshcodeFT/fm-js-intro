b4872f5dcd9c5d4aef5e438c1328ce1a367eb5f6
Nikolay <freshitftm@gmail.com>
2021-03-31 10:46:36 +0300

    Add prototype example

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,31 +17,24 @@ arr[arr.length] = 'test';
 arr.push(2);
 arr.push(7,8,9,10);
 
+const testArr = [];
+
 console.log(userTels);
 console.log(arr);
 
-function MyArray(){
-  this.length = 0;
-  this.push = function push(item) {
-    this[this.length++] = item;
-    return this.length;
-  }
+const myArrayProto = {};
+myArrayProto.push = function push(item) {
+  this[this.length++] = item;
+  return this.length;
 }
 
-const myArr = new MyArray();
+function MyArray(){
+  this.length = 0;
+}
 
-const t1 = 1;
-const t2 = 1;
+MyArray.prototype = myArrayProto;
 
-const obj1 = {divan: 'black'};
-const obj2 = {divan: 'black'};
 
-const link1 = obj1;
+const myArr1 = new MyArray();
 
-console.log(obj1 === obj2); // false
-console.log(link1 === obj2) // false
-console.log(obj1 === link1); // true
-
-link1.table = "brown";
-
-console.log(obj1);
+const myArr2 = new MyArray();

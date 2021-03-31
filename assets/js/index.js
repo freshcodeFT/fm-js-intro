e92b506538aaae714a20733b97217080454cf4ed
Nikolay <freshitftm@gmail.com>
2021-03-31 09:48:41 +0300

    Add array example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,20 @@
 'use strict';
 
+const userTel1 = '38042748374';
+const userTel2 = '38042748374';
+const userTel3 = '38042748374';
 
-/* 
-  Создать таблицу умножения
-  Разместить её в объекте. 
-  При этом ключ - само выражение, а значение - результат умножения
-*/
-function createMultiplicationTable(){
-  const table = {};
-  for(let i = 1; i <= 10; i++){
-    for(let j = 1; j <= 10; j++){
-      table[`${i} * ${j}`] = i * j;
-    }
-  }
+const userTels = {
+  0: '38089347595',
+  1: '38042748374',
+  2: '38454548376',
+};
 
-  return table;
-}
+userTels[3] = '38000348394';
+
+const arr = ['38089347595', '38042748374', '38454548376', '38000348394'];
+arr[4] = 'test';
+
+
+console.log(userTels);
+console.log(arr);

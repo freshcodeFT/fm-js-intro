dfc7e3ca1f49e171875a04cab7a202abdf294d9a
Nikolay <freshitftm@gmail.com>
2021-04-01 11:15:47 +0300

    Add Accumulator example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -36,3 +36,20 @@ driver2.__proto__ = car;
 что ввёл пользователь при вызовах метода read(),
 с учётом начального значения startingValue. 
 */
+
+function Accumulator(startingValue){
+  this.value = isFinite(startingValue) ? +startingValue : 0;
+}
+
+Accumulator.prototype = new AccumulatorPrototype();
+
+function AccumulatorPrototype(){
+  this.read = function read(){
+    const userNumber = +prompt("Введите число: ");
+    if(!isFinite(userNumber)) return this.value;
+
+    return this.value += userNumber;
+  }
+}
+
+const accumulator = new Accumulator();

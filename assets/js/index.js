55194aeab2060f151f528ae70fe1d86dc1c6c7a9
Nikolay <freshitftm@gmail.com>
2021-04-01 10:17:18 +0300

    Add student constructor example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,38 @@
 'use strict';
 
-const userTel1 = '38042748374';
-const userTel2 = '38042748374';
-const userTel3 = '38042748374';
-
-const userTels = {
-  0: '38089347595',
-  1: '38042748374',
-  2: '38454548376',
-};
-
-userTels[3] = '38000348394';
-
-const arrayTel = new Array('38089347595', '38042748374', '38454548376', '38000348394');
-const arr = ['38089347595', '38042748374', '38454548376', '38000348394'];
-
-arr[arr.length] = 'test';
-arr.push(2);
-arr.push(7,8,9,10);
-
-const testArr = [];
-
-console.log(userTels);
-console.log(arr);
-
-
-function MyArrayProto(){
-  this.push = function push() {
-    for (let i = 0; i < arguments.length; i++){
-      this[this.length++] = arguments[i];
-    }
-    return this.length;
-  }
-  this.pop = function pop(){
-    if(this.length === 0) return;
-
-    const lastValue = this[this.length-1];
-    delete this[--this.length];
-    return lastValue;
-  }
+const student1 = {
+  id: 1,
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 20,
+  isMale: true,
 }
 
-function MyArray(){
-  this.length = 0;
-  for (let i = 0; i < arguments.length; i++){
-    this.push(arguments[i]);
-  }
+function Student(firstName, lastName, age, isMale){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
 }
 
-MyArray.prototype = new MyArrayProto();
+Student.prototype = new StudentPrototype();
 
+function StudentPrototype(){
+  this.toString = function toString(){
+    return `${this.firstName} ${this.lastName}. I'm ${this.age} years old `;
+  }
+  this.sayHello = function sayHello(){
+    return `${this.firstName} ${this.lastName} says hello!`;
+  }
+  this.say = function say(message){
+    return `Студент ${this.firstName} говорит:\n${message}`;
+  }
+}
+/*
+  say() метод, принимающий строку и возвращающий строку
+  в таком формате:
+  Студент <Имя студента> говорит:
+  <содержимое принятой строки>
+*/
 
-const myArr1 = new MyArray();
-
-const myArr2 = new MyArray();
+const student2 = new Student('Bob', 'Black', 25, true);

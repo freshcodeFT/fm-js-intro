56acc282098afc24cb08b21eddcb32b71b1563a1
Nikolay <freshitftm@gmail.com>
2021-04-02 11:59:39 +0300

    Add square in forEach example

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,18 @@ users.forEach(subscribe);
 myArrUsers.forEach(function(elem){
   elem.isSubscribed = true;
 });
+
+const numsArray = [1,2,3,4,5];
+const numsMyAray = new MyArray(1,2,3,4,5);
+
+function square(currentNumber){
+
+  const result = currentNumber * currentNumber;
+  //console.log(result);
+  return result;
+}
+
+numsArray.forEach(square);
+numsMyAray.forEach(function(currentNumber, index, arr){
+  arr[index] = square(currentNumber);
+});

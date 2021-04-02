6feeb1e0fd092f59063ab5f40f80409d01b1becc
Nikolay <freshitftm@gmail.com>
2021-04-02 10:48:01 +0300

    Add writeLog

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -7,3 +7,25 @@ function saySomething(how, whatToSay){
   //console.log(whatToSay);
   how(whatToSay);
 }
+
+function sum(a,b){
+  return a + b;
+}
+
+function withLog(fn){
+  return function(...args){
+    const result = fn(...args);
+    console.log(result);
+    return result;
+  }
+}
+
+const sumWithLog = withLog(sum);
+
+const getFullName = function(name, surname){
+  return `${name} ${surname}`
+}
+
+const getFullNameWithLog = withLog(getFullName);
+
+const res = getFullNameWithLog('John', 'Smith');

e7ef3b81f8e4572938f49ae69770c4da0792dcf5
Nikolay <freshitftm@gmail.com>
2021-04-02 10:30:35 +0300

    Add high order function example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,33 +1,9 @@
-/* 
-  new Ladder(); - ЛЕСТНИЦА
-  value - та ступенька на который сейчас
-  up() - поднимает вас на одну ступеньку
-  down() - опускает на одну
-  showStep() - показывает на какой вы ступеньке
-
-  Сделать это возможным:
-  ladder.up().up().down().showStep(); // 1
-*/
-
-
-function Ladder(){
-  this.value = 0;
+/**
+ * 
+ * @param {function} how 
+ * @param {string} whatToSay 
+ */
+function saySomething(how, whatToSay){
+  //console.log(whatToSay);
+  how(whatToSay);
 }
-
-Ladder.prototype = new LadderPrototype();
-function LadderPrototype(){
-  this.up = function up(){
-    this.value++;
-    return this;
-  }
-  this.down = function down(){
-    this.value--;
-    return this;
-  }
-  this.showStep = function showStep(){
-    return this.value;
-  }
-}
-
-const ladder = new Ladder();
-

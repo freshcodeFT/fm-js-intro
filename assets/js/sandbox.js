e2bca15f2cff1cfa082650516bc859208a35fd10
Nikolay <freshitftm@gmail.com>
2021-04-02 10:12:44 +0300

    Add Ladder example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,55 +1,33 @@
+/* 
+  new Ladder(); - ЛЕСТНИЦА
+  value - та ступенька на который сейчас
+  up() - поднимает вас на одну ступеньку
+  down() - опускает на одну
+  showStep() - показывает на какой вы ступеньке
 
-// Написать сумму произвольного количества чисел
-function getSum(){
-  let result = null;
-  for(let i = 0; i < arguments.length; i++){
-    result += +arguments[i];
-  }
-
-  return result;
-}
-
-const car = {
-  go() {
-    return this.name;
-  }
-}
-
-const driver1 = {
-  name: 'John Smith',
-}
-
-const driver2 = {
-  name: 'John Doe',
-}
-
-driver1.__proto__ = car;
-driver2.__proto__ = car;
-
-/*
-Напишите функцию-конструктор Accumulator(startingValue).
-Объект, который она создаёт, должен уметь следующее:
-Хранить «текущее значение» в свойстве value. 
-Начальное значение устанавливается в аргументе конструктора startingValue.
-Метод read() использует prompt для получения числа и прибавляет его к свойству value.
-Таким образом, свойство value является текущей суммой всего,
-что ввёл пользователь при вызовах метода read(),
-с учётом начального значения startingValue. 
+  Сделать это возможным:
+  ladder.up().up().down().showStep(); // 1
 */
 
-function Accumulator(startingValue){
-  this.value = isFinite(startingValue) ? +startingValue : 0;
+
+function Ladder(){
+  this.value = 0;
 }
 
-Accumulator.prototype = new AccumulatorPrototype();
-
-function AccumulatorPrototype(){
-  this.read = function read(){
-    const userNumber = +prompt("Введите число: ");
-    if(!isFinite(userNumber)) return this.value;
-
-    return this.value += userNumber;
+Ladder.prototype = new LadderPrototype();
+function LadderPrototype(){
+  this.up = function up(){
+    this.value++;
+    return this;
+  }
+  this.down = function down(){
+    this.value--;
+    return this;
+  }
+  this.showStep = function showStep(){
+    return this.value;
   }
 }
 
-const accumulator = new Accumulator();
+const ladder = new Ladder();
+

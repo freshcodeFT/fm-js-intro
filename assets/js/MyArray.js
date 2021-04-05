9be84593910c8e3efd27b6091b80b7a32d42ba7d
Nikolay <freshitftm@gmail.com>
2021-04-05 11:37:21 +0300

    Add some example

--- assets/js/MyArray.js
+++ assets/js/MyArray.js
@@ -1,43 +1,43 @@
-function MyArrayProto(){
+function MyArrayProto() {
   this.push = function push() {
-    for (let i = 0; i < arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++) {
       this[this.length++] = arguments[i];
     }
     return this.length;
-  }
-  this.pop = function pop(){
-    if(this.length === 0) return;
+  };
+  this.pop = function pop() {
+    if (this.length === 0) return;
 
-    const lastValue = this[this.length-1];
+    const lastValue = this[this.length - 1];
     delete this[--this.length];
     return lastValue;
-  }
-  this.unshift = function unshift(){
-    for(let i = this.length - 1; i >= 0; i--){
+  };
+  this.unshift = function unshift() {
+    for (let i = this.length - 1; i >= 0; i--) {
       this[i + arguments.length] = this[i];
     }
-    for(let i = 0; i < arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++) {
       this[i] = arguments[i];
     }
-    return this.length += arguments.length;
-  }
-  this.shift = function shift(){
-    if(this.length === 0) return;
+    return (this.length += arguments.length);
+  };
+  this.shift = function shift() {
+    if (this.length === 0) return;
     const firstElem = this[0];
-    for(let i = 0; i < this.length - 1; i++){
-      this[i] = this[i + 1]
+    for (let i = 0; i < this.length - 1; i++) {
+      this[i] = this[i + 1];
     }
     delete this[--this.length];
     return firstElem;
-  }
-  this.concat = function concat(){
+  };
+  this.concat = function concat() {
     const newArray = new MyArray();
-    for(let i = 0; i < this.length; i++){
+    for (let i = 0; i < this.length; i++) {
       newArray.push(this[i]);
     }
-    for(let i = 0; i < arguments.length; i++){
-      if(MyArray.isMyArray(arguments[i]) || Array.isArray(arguments[i])){
-        for(let j = 0; j < arguments[i].length; j++){
+    for (let i = 0; i < arguments.length; i++) {
+      if (MyArray.isMyArray(arguments[i]) || Array.isArray(arguments[i])) {
+        for (let j = 0; j < arguments[i].length; j++) {
           newArray.push(arguments[i][j]);
         }
       } else {
@@ -45,45 +45,52 @@ function MyArrayProto(){
       }
     }
     return newArray;
-  }
-  this.reverse = function reverse(){
+  };
+  this.reverse = function reverse() {
     const maxIndex = this.length - 1;
     const middle = maxIndex / 2;
-    for(let i = 0; i < middle; i++){
+    for (let i = 0; i < middle; i++) {
       const temp = this[i];
       this[i] = this[maxIndex - i];
       this[maxIndex - i] = temp;
     }
     return this;
-  }
+  };
   /**
-   * 
-   * @param {function} callback 
+   *
+   * @param {function} callback
    */
-  this.forEach = function forEach(callback){
-    for( let i = 0; i < this.length; i++){
+  this.forEach = function forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
       callback(this[i], i, this);
     }
-  }
-
-  this.map = function map(callback){
+  };
+  this.map = function map(callback) {
     const newArray = new MyArray();
-    for( let i = 0; i < this.length; i++){
+    for (let i = 0; i < this.length; i++) {
       newArray.push(callback(this[i], i, this));
     }
     return newArray;
-  }
+  };
+  this.some = function some(callback) {
+    for (let i = 0; i < this.length; i++) {
+      if(callback(this[i], i, this)){
+        return true;
+      }
+    }
+    return false;
+  };
 }
 
-function MyArray(){
+function MyArray() {
   this.length = 0;
-  for (let i = 0; i < arguments.length; i++){
+  for (let i = 0; i < arguments.length; i++) {
     this.push(arguments[i]);
   }
 }
 MyArray.prototype = new MyArrayProto();
-MyArray.__proto__.isMyArray = function isMyArray(arr){
+MyArray.__proto__.isMyArray = function isMyArray(arr) {
   return arr instanceof MyArray;
-}
+};
 
-console.dir(MyArray)
+console.dir(MyArray);
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,35 @@
-'use strict';
+"use strict";
 
-const users = [{},{},{},{},{}];
-const myArrUsers = new MyArray({},{},{},{},{});
+const users = [{}, {}, {}, {}, {}];
+const myArrUsers = new MyArray({}, {}, {}, {}, {});
 
-const subscribe = function(elem){
+const subscribe = function (elem) {
   elem.isSubscribed = true;
-}
+};
 
 users.forEach(subscribe);
 
-myArrUsers.forEach(function(elem){
+myArrUsers.forEach(function (elem) {
   elem.isSubscribed = true;
 });
 
-const numsArray = [1,2,3,4,5];
-const numsMyAray = new MyArray(1,2,3,4,5);
+const numsArray = [1, 2, 3, 4, 5];
+const numsMyAray = new MyArray(1, 2, 3, 4, 5);
 
-function square(currentNumber){
-
+function square(currentNumber) {
   const result = currentNumber * currentNumber;
   //console.log(result);
   return result;
 }
 
+function isEven(num) {
+  return num % 2 === 0;
+}
+
 numsArray.forEach(square);
-numsMyAray.forEach(function(currentNumber, index, arr){
+numsMyAray.forEach(function (currentNumber, index, arr) {
   arr[index] = square(currentNumber);
 });
 
-const myArray = new MyArray(1,2,3,4,5,6,7, 8);
+//const myArray = new MyArray(1, 2, 3, 4, 5, 6, 7, 8);
+const myArray = new MyArray(1, 3, 5, 7);

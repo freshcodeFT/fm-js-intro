72db50f452799b2e0eace1eccf5ec2fe653dea84
Nikolay <freshitftm@gmail.com>
2021-04-06 11:21:14 +0300

    Add sum

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,10 +11,18 @@ const t2 = () => {
 t1(1,2,3,4,5)
 // t2(1,2,3,4,5)
 
-function sum(){
+function sumArgs(){
   let res = null;
   for(let i = 0; i < arguments.length; i++){
     res += arguments[i];
+  }
+  return res;
+}
+
+const sumArrow = (...args) => {
+  let res = null;
+  for(let i = 0; i < args.length; i++){
+    res += args[i];
   }
   return res;
 }

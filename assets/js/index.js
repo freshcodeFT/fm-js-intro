73e88b5f3044726cdfcced251e2740a68c63329b
Nikolay <freshitftm@gmail.com>
2021-04-06 10:01:47 +0300

    Add gerRandomUsers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,8 @@
 "use strict";
 
-const users = [{}, {}, {}, {}, {}];
-const myArrUsers = new MyArray({}, {}, {}, {}, {});
-
 const subscribe = function (elem) {
   elem.isSubscribed = true;
 };
-
-users.forEach(subscribe);
-
-myArrUsers.forEach(function (elem) {
-  elem.isSubscribed = true;
-});
-
-const numsArray = [1, 2, 3, 4, 5];
-const numsMyAray = new MyArray(1, 2, 3, 4, 5);
 
 function square(currentNumber) {
   const result = currentNumber * currentNumber;
@@ -22,18 +10,53 @@ function square(currentNumber) {
   return result;
 }
 
-function isEven(num) {
-  return num % 2 === 0;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function isOdd(num) {
-  return num % 2 !== 0;
+const numsArr = [];
+for (let i = 0; i < 10; i++) {
+  numsArr.push(getRandomIntInclusive(100, 200));
 }
 
-numsArray.forEach(square);
-numsMyAray.forEach(function (currentNumber, index, arr) {
-  arr[index] = square(currentNumber);
-});
+function User(firstName, lastName, email, age, isMale, isSubscribed = false) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.age = age;
+  this.isMale = isMale;
+  this.isSubscribed = isSubscribed;
+}
 
-//const myArray = new MyArray(1, 2, 3, 4, 5, 6, 7, 8);
-const myArray = new MyArray(1, 2, 3, 4, 5, 7);
+User.prototype = new UserProto();
+
+function UserProto(){
+  this.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+/* 
+  1. Создать прототип для User. 
+  2. Создать ф-ю getFullName, которая вернет строку с  именем и фамилией пользователя
+  3. Добавить рандомную генерацию подписок (значение поля isSubscribed)
+*/
+
+function getRandomUsers(amount) {
+  const usersArray = [];
+  for (let i = 0; i < amount; i++) {
+    usersArray.push(
+      new User(
+        `Name${i}`, //first name
+        `Surname${i}`, //last name
+        `user${i}@gmail.com`, //email
+        getRandomIntInclusive(12, 50), //age
+        Math.random() < 0.5, //isMale
+        Math.random() < 0.5 //isSubscribed
+      )
+    );
+  }
+  return usersArray;
+}

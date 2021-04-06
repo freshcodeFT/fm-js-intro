fcb4db08c4ed2bbce5b21a69955d781fcd9deefc
Nikolay <freshitftm@gmail.com>
2021-04-06 12:01:15 +0300

    Add spread example

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,12 @@ const t2 = () => {
 t1(1, 2, 3, 4, 5);
 // t2(1,2,3,4,5)
 
+
+const t3 = (a,b,...args) => {
+  console.log(a,b);
+  console.log(...args);
+}
+
 function sumArgs() {
   let res = null;
   for (let i = 0; i < arguments.length; i++) {
@@ -32,3 +38,8 @@ const sumArrowReduce = (...rest) => {
     return accumulator + currentValue;
   });
 };
+
+const numbers = [1, 1, 32, 15, 122, 1537, 5, 4, 8];
+const numbers2 = [1234, 1324, 43, 4567, 7];
+
+const numbers3 = [...numbers, ...numbers2];

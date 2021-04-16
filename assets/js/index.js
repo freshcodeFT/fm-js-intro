58bdbc6d61bbd7b297309e17ee15c7718f27889e
Nikolay <freshitftm@gmail.com>
2021-04-16 11:52:33 +0300

    translate refactor

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,31 +2,29 @@
 
 const vocabulary = new Map();
 
-vocabulary.set('cat', 'кот');
-vocabulary.set('dog', 'собака');
-vocabulary.set('table', 'стол');
-vocabulary.set('screen', 'экран');
-vocabulary.set('structure', 'структура');
-vocabulary.set('algorithm', 'алгоритм');
-vocabulary.set('count', 'считать');
-vocabulary.set('set', 'установить');
-vocabulary.set('kernel', 'ядро');
-vocabulary.set('queue', 'очередь');
+vocabulary.set("cat", "кот");
+vocabulary.set("dog", "собака");
+vocabulary.set("table", "стол");
+vocabulary.set("screen", "экран");
+vocabulary.set("structure", "структура");
+vocabulary.set("algorithm", "алгоритм");
+vocabulary.set("count", "считать");
+vocabulary.set("set", "установить");
+vocabulary.set("kernel", "ядро");
+vocabulary.set("queue", "очередь");
 
-const inputStr = 'Cat counT Dog set queue Kernel algOrithm strUcTure of the TABLE';
+const inputStr =
+  "Cat counT Dog set queue Kernel algOrithm strUcTure of the TABLE";
 
 const translate = (str) => {
-  const lowerString = str.toLowerCase();
-  const splitStrArray = lowerString.split(' ');
-  const translatedStringArray = splitStrArray.map((word)=>{
-    if(vocabulary.has(word)){
-      return vocabulary.get(word);
-    }
-    return word;
-  });
-  const translatedString = translatedStringArray.join(' ');
-
-  return translatedString;
-}
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return vocabulary.has(word) ? vocabulary.get(word) : word;
+    })
+    .join(" ");
+};
 
 console.log(translate(inputStr));

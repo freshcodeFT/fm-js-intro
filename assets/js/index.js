1c417491822c43af353068fdc2881b03e5b1d5b9
Nikolay <freshitftm@gmail.com>
2021-04-16 11:49:23 +0300

    Add Vocabulary

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,32 @@
 "use strict";
 
-const mySymbol = Symbol('test');
+const vocabulary = new Map();
 
-const obj = {
-  test: 'test',
-  [mySymbol]: 5
-};
+vocabulary.set('cat', 'кот');
+vocabulary.set('dog', 'собака');
+vocabulary.set('table', 'стол');
+vocabulary.set('screen', 'экран');
+vocabulary.set('structure', 'структура');
+vocabulary.set('algorithm', 'алгоритм');
+vocabulary.set('count', 'считать');
+vocabulary.set('set', 'установить');
+vocabulary.set('kernel', 'ядро');
+vocabulary.set('queue', 'очередь');
 
-const obj2 = {
-  test: 'test',
-  [mySymbol]: 5
-};
+const inputStr = 'Cat counT Dog set queue Kernel algOrithm strUcTure of the TABLE';
 
-const myMap = new Map();
-myMap.set(1, {});
-myMap.set('1', true);
-myMap.set(1, 10000);
-myMap.set(obj, 'testValue');
-myMap.set(obj2, 'anotherValue');
+const translate = (str) => {
+  const lowerString = str.toLowerCase();
+  const splitStrArray = lowerString.split(' ');
+  const translatedStringArray = splitStrArray.map((word)=>{
+    if(vocabulary.has(word)){
+      return vocabulary.get(word);
+    }
+    return word;
+  });
+  const translatedString = translatedStringArray.join(' ');
 
-console.log(myMap.get(1));
+  return translatedString;
+}
+
+console.log(translate(inputStr));

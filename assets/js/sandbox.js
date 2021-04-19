964fc7f87ee6a812192b91f1ea585845b2e57356
Nikolay <freshitftm@gmail.com>
2021-04-19 11:39:36 +0300

    Add destructure

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,57 +1,49 @@
-function checkAnagram(word1, word2) {
-  return (
-    word1.toLowerCase().split("").sort().join("") ===
-    word2.toLowerCase().split("").sort().join("")
-  );
-}
-
-function createMap(str) {
-  const mapToStr = new Map();
-  for (const letter of str) {
-    if (mapToStr.has(letter)) {
-      const letterCount = mapToStr.get(letter);
-      mapToStr.set(letter, letterCount + 1);
-      continue;
-    }
-    mapToStr.set(letter, 1);
-  }
-  return mapToStr;
-}
-
-function compare(word1, word2) {
-  if (word1.length !== word2.length) {
-    return false;
-  }
-  const mapToStr1 = createMap(word1);
-  const mapToStr2 = createMap(word2);
-
-  if (mapToStr1.size !== mapToStr2.size) {
-    return false;
-  }
-
-  for (const key of mapToStr1.keys()) {
-    if (mapToStr1.get(key) !== mapToStr2.get(key)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function countVowels(str) {
-  return str
-    .toLowerCase()
-    .split("")
-    .reduce((accumulator, currentValue) => {
-      return VOWELS.includes(currentValue) ? accumulator + 1 : accumulator;
-    }, 0);
-}
-
 function uniqueizeArray(arr) {
-  if(!Array.isArray(arr)){
+  if (!Array.isArray(arr)) {
     throw new TypeError();
   }
   return arr.filter((item, index, array) => {
     return array.indexOf(item) === index;
   });
 }
+
+const mySet = new Set([1, 2, 3, 4, 5, 6, 7, 8]);
+
+const nums1 = [1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 44, 5, 6];
+const nums2 = [1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 0, 0];
+
+// Создать массив, содержащий уникальные элменты из предыдущих массивов
+
+const unique = [...new Set([...nums1, ...nums2])];
+
+const monitor = {
+  sizes: {
+    height: {
+      value: 20,
+      scale: "cm",
+    },
+    width: {
+      value: 56.5,
+      scale: "cm",
+    },
+  },
+  model: "Samsung",
+  brightness: 200,
+  contrast: 100,
+  color: "black",
+  dpi: 250,
+  resolution: "4K",
+};
+
+console.log(monitor.sizes.height.value);
+
+function getDiagonal(monitor) {
+  monitor.sizes.height.value;
+  monitor.sizes.width.value;
+}
+
+// const monitorBright = monitor.brightness;
+
+const { brightness, dpi, color } = monitor;
+
+console.log(brightness, dpi, color);

7668b1caf2c489b3d88b65232926c6a51d0b0045
Nikolay <freshitftm@gmail.com>
2021-04-20 12:08:33 +0300

    Add numGuesser

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,3 +50,22 @@ const createMultiplicationTable = (limit = 10) => {
   }
   return table;
 }
+
+
+const numGuesser = (startValue = 0, endValue = 100) => {
+  let start = startValue;
+  let end = endValue;
+  let middle = Math.round((start + end) / 2);
+  while(true) {
+    if(confirm(`Ваше число ${middle}?`)) {
+      return middle;
+    }
+    if (confirm(`${middle} больше чем ваше число?`)){
+      end = middle;
+      middle = Math.floor((start + end) / 2);
+    } else {
+      start = middle;
+      middle = Math.ceil((start + end) / 2);
+    }
+  }
+}
